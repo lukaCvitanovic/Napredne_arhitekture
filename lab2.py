a3e3f35077bbab73d6d716a5c25a502baf7de267
lukaCvitanovic <lcvita01@fesb.hr>
2019-06-02 20:20:47 +0200

    lab2 changes

--- lab2.py
+++ lab2.py
@@ -3,6 +3,7 @@ import random
 import sys
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 
 def generateData(kb):
     size = kb * 1024
@@ -11,10 +12,11 @@ def generateData(kb):
         numbers.append(random.randint(0, 101))
     return numbers
 
-def calculateAverage(numbers):
+def calculateAverage(numbers, offset):
     sum = 0
     for i in range(0, len(numbers)):
         sum += numbers[i]
+    print(offset)
     return sum/len(numbers)
 
 def createHistogram(data):
@@ -28,22 +30,26 @@ def createHistogram(data):
     plt.bar(np.arange(len(times)), times)
     plt.xticks(np.arange(len(times)), sizes)
     plt.xlabel('Velićina podataka u KB')
-    plt.ylabel('Vrijeme u sekundama')
+    plt.ylabel('Vrijeme u tikovima')
     plt.show()
 
 def doWork():
-    Size = 128
+    Size = 1
     times = list()
 
-    for i in range(1,9):
+    for i in range(1,16):
         print("run: " + str(i))
         print("Generating data...")
         numbers = generateData(Size)
         print("Size of numbers: " + str(sys.getsizeof(numbers)/1024) + "in KB")
         print("Calculating...")
-        start = time.time()
-        calculateAverage(numbers)
-        end = time.time()
+        start = time.clock()
+        k=0
+        for k in range(0,(16-i)):
+            calculateAverage(numbers, k)
+        #print(k)
+        #calculateAverage(numbers, k)
+        end = time.clock()
         size = Size
         times.append({"time":(end-start), "size":size})
         Size *= 2
